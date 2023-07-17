2ea9482bdc8e6495764dcb4f5ea6ca16c2099d64
samiracode <samiratavakolli.k@gmail.com>
2023-07-17 22:15:14 +0200

    Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { menu, close } from '../assets';
+import { logost } from '../assets';
+
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+
   return (
-    <div>Navbar</div>
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+         <Link 
+           to="/"
+           className="flex items-center gap-2"
+           onClick={ () => {
+             setActive("");
+             window.scrollTo(0, 0)
+           }}
+          >
+            <img src={logost} alt='logo' className='w-9 h-9 object-contain'/>
+            <p 
+             className='text-white text-[18px] font-bold cursor-pointer'
+            >
+              Samira Tavakolli <span className='sm:block hidden'>Software Developer</span>
+            </p>
+         </Link>
+      </div>
+    </nav>
   )
 }
 
