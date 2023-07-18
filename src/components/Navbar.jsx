83749831a5989e1cb5992bde622047f81e543a5e
samiracode <samiratavakolli.k@gmail.com>
2023-07-18 10:41:46 +0200

    margin left to navbar non zero & samira software

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary `}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
          <Link 
            to="/"
@@ -21,13 +21,19 @@ const Navbar = () => {
              window.scrollTo(0, 0)
            }}
           >
-            <img src={logost} alt='logo' className='w-9 h-9 object-contain'/>
-            <p 
-             className='text-white text-[18px] font-bold cursor-pointer'
-            >
-              Samira Tavakolli <span className='sm:block hidden'>Software Developer</span>
+            <img src={logost} alt='logost' className='w-9 h-9 object-contain flex'/>
+            <p className='text-white text-[18px] font-bold cursor-pointer flex'>
+              Samira 
+              <span className='sm:block hidden'> | Software Developer</span>
             </p>
          </Link>
+         <ul className='list-none hidden sm:flex flex-row gap-10'>
+            {navLinks.map((link) => (
+              <li>
+                <a href={`#${link.id}`}>{link.title}</a>
+              </li>
+            ) )}
+         </ul>
       </div>
     </nav>
   )
