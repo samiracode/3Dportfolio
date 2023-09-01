750331f681f8a2b7abfa7534ae2bf6762b5a5f50
Samira Tavakolli <samiratavakolli.k@gmail.com>
2023-09-01 17:21:14 +0200

    tertiary changed to #171407

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
       colors: {
         primary: "#050816",
         secondary: "#eed14d",
-        tertiary: "#5f531e",
+        tertiary: "#171407",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
