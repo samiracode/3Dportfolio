4e5e41d1251d4d1e04ee86145dbe6085cf7707d3
Samira Tavakolli <samiratavakolli.k@gmail.com>
2023-09-01 17:17:39 +0200

    #aaa6c3 to #eed14d in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
     extend: {
       colors: {
         primary: "#050816",
-        secondary: "#aaa6c3",
-        tertiary: "#151030",
+        secondary: "#eed14d",
+        tertiary: "#5f531e",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
