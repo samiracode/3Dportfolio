9dc12dce088a7841dbc2f2f8bb0f0d937c36c30e
Samira Tavakolli <samiratavakolli.k@gmail.com>
2024-01-16 16:05:40 +0100

    overview

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -48,12 +48,13 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with background in Archtiecture engineering.
-        I have experience in TypeScript and JavaScript,
-        and expertise in frameworks like React and Node.js.
-        I'm a quick learner and collaborate closely with clients to
+        I'm a software developer with background in Archtiectural Engineering.
+        I have experience in JavaScript, TypeScript and Python
+        with a hand-on frameworks like React, Node.js and Django.
+        I'm a quick learner and collaborate closely with clients or team members to
         create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        real-world problems. Go ahead and take a look at my experience and projects
+        for knowing me better!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
